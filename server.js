94a9678dd8b564767a1326815f33238fd894c65d
satish <satishkonduru0225@gmail.com>
2025-05-23 07:18:03 +0530

    small update, removed console from server.js

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 // Server Listeninng
 app.listen(PORT, () => {
-  console.log("TESTING..........");
   console.log(`Server is Running at PORT: ${PORT}`);
 });

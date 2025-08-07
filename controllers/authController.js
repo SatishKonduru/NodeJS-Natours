aa23337dfcec4be297edf6f86ba4f308ced3b39e
satish <satishkonduru0225@gmail.com>
2025-08-07 07:20:25 +0530

    SignIn

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,14 @@
+const AppError = require("../utils/appError");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+
+const signToken = (id) => {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   // const newUser = await User.create(req.body);
@@ -11,9 +18,10 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  // const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+  //   expiresIn: process.env.JWT_EXPIRES_IN,
+  // });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: "success",
@@ -21,5 +29,31 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  //Check if email and password exist in DB
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password", 400));
+  }
+  console.log("User provided email and password: ", email, password);
+  // Check if user exists & password is correct
+  const user = User.findOne({ email: email }).select("+password");
+  console.log("xxxxxxxxxxxxxx", user.email);
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    console.log("DDDDDDDDDDDDDDDDDDD");
+    return next(new AppError("Incorrect email or Incorrect Password", 401));
+  }
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or Password", 401));
+  }
+  // if everything is OK, send token to client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: "success",
+    token,
   });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,11 +5,15 @@ const router = express.Router();
 
 // User Routes
 router.post('/signup', authController.signup)
+router.post("/login", authController.login);
+
 // Tour Routes
 router
   .route("/")
   .get(userController.getAllUsers)
   .post(userController.createUser);
+
+
 
 router
   .route("/:id")

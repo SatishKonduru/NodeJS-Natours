9dd702964fac31a617a85faeb11aa47a8013c317
satish <satishkonduru0225@gmail.com>
2025-08-29 18:07:54 +0530

    UpdateMe

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
+const AppError = require("../utils/appError");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -12,7 +21,40 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateMe = catchAsync(async (req, res, next) => {});
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1. Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use ' /updateMyPassword '  ",
+        400
+      )
+    );
+  }
+
+  // 2. Filter out unwanted fields from req.body
+  const filteredBody = filterObj(req.body, "name", "email");
+
+  // 3. Check if email is already exist
+  if (filteredBody.email) {
+    const existingUser = await User.findOne({ email: filteredBody.email });
+    if (existingUser && existingUser.id !== req.body.id) {
+      return next(new AppError("Email is already in use by another user", 400));
+    }
+  }
+
+  // 4. update the user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+});
 
 exports.getUser = (req, res) => {};
 
